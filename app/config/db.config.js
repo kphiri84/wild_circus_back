ee2645ab6d79d2ed60c8bbe21500412267ddff33
kphiri84 <kenny.phiri@laposte.net>
2020-09-30 13:02:36 +0200

    heroku deploiement

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "ken2ken842",
-    DB: "circus2",
+    HOST: "eu-cdbr-west-03.cleardb.net",
+    USER: "b84021cb35f954",
+    PASSWORD: "91104b95",
+    DB: "heroku_0e7f1878f7bf5e8",
     dialect: "mysql",
     pool: {
         max: 5,
